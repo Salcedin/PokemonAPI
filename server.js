b25a23298f8acd482bf3ff71a93f946ac4c343f4
Salcedin <billy.talent01@gmail.com>
2024-01-31 20:34:55 +0100

    movidas

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors'); // Agrega la importación de cors
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors()); // Usa cors antes de definir tus rutas
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 app.use('/scripts', express.static('public/scripts', { 'Content-Type': 'application/javascript' }));
