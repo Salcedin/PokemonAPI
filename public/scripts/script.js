de99c10c61fe32af2860ae7447347d77d92e404d
Salcedin <billy.talent01@gmail.com>
2024-01-31 20:06:57 +0100

    first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+    var capturar = $('#capturar');
+    var resultado = $('#resultado');
+    var pokemonInput = $('#pokemonInput');
+
+    function getPokemonData() {
+        var pokemonName = pokemonInput.val().toLowerCase();
+
+        if (pokemonName) {
+            PokemonPorNombre(pokemonName);
+        } else {
+            alert('Por favor, ingresa el nombre de un Pokémon.');
+        }
+    }
+
+    // Obtener datos al presionar "Enter"
+    pokemonInput.keypress(function (e) {
+        if (e.which === 13) {
+            getPokemonData();
+        }
+    });
+
+    capturar.click(function () {
+        getPokemonData();
+    });
+
+    $('.typeBtn').click(function () {
+        var pokemonType = $(this).data('type');
+        PokemonPorTipo(pokemonType);
+    });
+
+    function mostrarResultados(data) {
+        resultado.empty();
+        resultado.append('<h2>Datos del Pokémon:</h2>');
+
+        var formattedData = 'Nombre: ' + data.name + '\nID: ' + data.id + '\nTipo: ' + data.types[0].type.name;
+        var spriteUrl = data.sprites.front_default;
+
+        resultado.append('<img src="' + spriteUrl + '" alt="' + data.name + '">');
+        resultado.append('<pre>' + formattedData + '</pre>');
+    }
+
+    function PokemonPorTipos(data) {
+        resultado.empty();
+        resultado.append('<h2>Pokémon del tipo ' + data[0].type + ':</h2>');
+
+        // Crear tarjetas para cada Pokémon
+        data.forEach(function (pokemon) {
+            var card = '<div class="resultado-carta">' +
+                '<img src="' + pokemon.sprite + '" alt="' + pokemon.name + '">' +
+                '<p>ID: ' + pokemon.id + '</p>' +
+                '<p>Nombre: ' + pokemon.name + '</p>' +
+                '<p>Tipo: ' + pokemon.type + '</p>' +
+                '</div>';
+
+                resultado.append(card);
+        });
+    }
+
+    async function PokemonPorNombre(pokemonName) {
+        try {
+            const apiResponse = await $.ajax({
+                url: 'http://localhost:3000/api-data/' + pokemonName,
+                type: 'GET',
+                dataType: 'json'
+            });
+
+            mostrarResultados(apiResponse);
+        } catch (error) {
+            console.error('Error en la petición al servidor:', error);
+        }
+    }
+
+    async function PokemonPorTipo(pokemonType) {
+        try {
+            const apiResponse = await $.ajax({
+                url: 'http://localhost:3000/api-data/type/' + pokemonType,
+                type: 'GET',
+                dataType: 'json'
+            });
+
+            PokemonPorTipos(apiResponse);
+        } catch (error) {
+            console.error('Error en la petición al servidor:', error);
+        }
+    }
+});
---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/scripts', express.static('public/scripts', { 'Content-Type': 'application/javascript' }));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api-data/:pokemonName', async (req, res) => {
+    try {
+        const pokemonName = req.params.pokemonName;
+        const apiResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        
+        res.json(apiResponse.data);
+    } catch (error) {
+        console.error('Error en la petición a la API:', error);
+        res.status(500).json({ error: 'Error en la petición a la API' });
+    }
+});
+
+app.get('/api-data/type/:type', async (req, res) => {
+    try {
+        const type = req.params.type;
+        const respuestaAPI = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
+        
+        // Extraer los detalles de los Pokémon del tipo desde la respuesta de la API
+        const pokemonDetails = respuestaAPI.data.pokemon.map(pokemon => ({
+            id: pokemon.pokemon.url.split('/').reverse()[1],
+            name: pokemon.pokemon.name,
+            type: type,
+            sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.pokemon.url.split('/').reverse()[1]}.png`
+        }));
+
+        res.json(pokemonDetails);
+    } catch (error) {
+        console.error('Error en la petición a la API:', error);
+        res.status(500).json({ error: 'Error en la petición a la API' });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto 3000`);
+});
